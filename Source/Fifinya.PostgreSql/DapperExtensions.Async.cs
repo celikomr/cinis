ff5c8492c959a0c9d78ed9b71f72791c2d6bd5cd
Ömer Çelik <oclk@outlook.com.tr>
2022-10-21 16:16:59 +0300

    feat: Developed CreateAsync func in Fifinya.PostgreSql project

--- Source/Fifinya.PostgreSql/DapperExtensions.Async.cs
+++ Source/Fifinya.PostgreSql/DapperExtensions.Async.cs
@@ -1,5 +1,23 @@
-﻿namespace Fifinya.PostgreSql;
+﻿using Dapper;
+using Npgsql;
+using System.Data;
+
+namespace Fifinya.PostgreSql;
 
 public static partial class DapperExtensions
 {
+    public static async Task<dynamic> CreateAsync<T>(this NpgsqlConnection connection, T entity, NpgsqlTransaction? transaction = null, DbType dbType = DbType.Int32)
+    {
+        if (connection is null)
+        {
+            throw new ArgumentNullException(nameof(connection));
+        }
+
+        var stringOfColumns = string.Join(", ", GetColumns<T>());
+        var stringOfParameters = string.Join(", ", GetColumnPropertyNames<T>().Select(e => "@" + e));
+        var sql = $"insert into {GetTableSchema<T>()}.{GetTableName<T>()} ({stringOfColumns}) values ({stringOfParameters}) returning {GetPrimaryKey<T>()?.Name}";
+
+        var result = await connection.ExecuteAsync(sql, entity, transaction);
+        return result;
+    }
 }
