98ce6a10a026a64446be1f2a73650b1525c3df71
Ömer Çelik <oclk@outlook.com.tr>
2022-10-21 16:08:48 +0300

    feat: Developed Create func in Fifinya.PostgreSql project

--- Source/Fifinya.PostgreSql/DapperExtensions.cs
+++ Source/Fifinya.PostgreSql/DapperExtensions.cs
@@ -1,5 +1,44 @@
-﻿namespace Fifinya.PostgreSql;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Data;
+using Npgsql;
+using Dapper;
+
+namespace Fifinya.PostgreSql;
 
 public static partial class DapperExtensions
 {
+    private static string GetTableName<T>()
+        => typeof(T).GetCustomAttribute<TableAttribute>().Name;
+
+    private static string GetTableSchema<T>()
+        => typeof(T).GetCustomAttribute<TableAttribute>().Schema;
+
+    private static IEnumerable<PropertyInfo> GetProperties<T>()
+        => typeof(T).GetProperties();
+
+    private static PropertyInfo? GetPrimaryKey<T>()
+        => typeof(T).GetProperties().Where(x => x.GetCustomAttributes().Any(y => y.GetType() == typeof(KeyAttribute))).FirstOrDefault();
+
+    private static IEnumerable<string?> GetColumns<T>()
+        => typeof(T).GetProperties().Where(e => e.Name != GetPrimaryKey<T>()?.Name && e.GetCustomAttribute<ColumnAttribute>() != null).Select(e => e.GetCustomAttribute<ColumnAttribute>()?.Name);
+
+    private static IEnumerable<string> GetColumnPropertyNames<T>()
+        => typeof(T).GetProperties().Where(e => e.Name != GetPrimaryKey<T>()?.Name && e.GetCustomAttribute<ColumnAttribute>() != null).Select(e => e.Name);
+
+    public static dynamic Create<T>(this NpgsqlConnection connection, T entity, NpgsqlTransaction? transaction = null, DbType dbType = DbType.Int32)
+    {
+        if (connection is null)
+        {
+            throw new ArgumentNullException(nameof(connection));
+        }
+
+        var stringOfColumns = string.Join(", ", GetColumns<T>());
+        var stringOfParameters = string.Join(", ", GetColumnPropertyNames<T>().Select(e => "@" + e));
+        var sql = $"insert into {GetTableSchema<T>()}.{GetTableName<T>()} ({stringOfColumns}) values ({stringOfParameters}) returning {GetPrimaryKey<T>()?.Name}";
+
+        var result = connection.Execute(sql, entity, transaction);
+        return result;
+    }
 }
