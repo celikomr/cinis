b6ebf616ab14a01beef320320c5f168fdd1af93a
Ömer Çelik <oclk@outlook.com.tr>
2022-10-21 14:02:22 +0300

    feat: Added general definitions in DapperExtensions file

--- Source/Fifinya.Oracle/DapperExtensions.cs
+++ Source/Fifinya.Oracle/DapperExtensions.cs
@@ -1,5 +1,26 @@
-﻿namespace Fifinya.Oracle;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Fifinya.Oracle;
 
 public static partial class DapperExtensions
 {
+    private static string GetTableName<T>()
+        => typeof(T).GetCustomAttribute<TableAttribute>().Name;
+
+    private static string GetTableSchema<T>()
+        => typeof(T).GetCustomAttribute<TableAttribute>().Schema;
+
+    private static IEnumerable<PropertyInfo> GetProperties<T>()
+        => typeof(T).GetProperties();
+
+    private static PropertyInfo? GetPrimaryKey<T>()
+        => typeof(T).GetProperties().Where(x => x.GetCustomAttributes().Any(y => y.GetType() == typeof(KeyAttribute))).FirstOrDefault();
+
+    private static IEnumerable<string?> GetColumns<T>()
+        => typeof(T).GetProperties().Where(e => e.Name != GetPrimaryKey<T>()?.Name && e.GetCustomAttribute<ColumnAttribute>() != null).Select(e => e.GetCustomAttribute<ColumnAttribute>()?.Name);
+
+    private static IEnumerable<string> GetColumnPropertyNames<T>()
+        => typeof(T).GetProperties().Where(e => e.Name != GetPrimaryKey<T>()?.Name && e.GetCustomAttribute<ColumnAttribute>() != null).Select(e => e.Name);
 }
