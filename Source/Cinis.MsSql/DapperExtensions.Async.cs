899176547f0f9d3964f3a480e531517abeafd9fa
Ömer Çelik <oclk@outlook.com.tr>
2022-10-23 13:50:56 +0300

    feat: Developed CreateAsync func in Cinis.MsSql project

--- Source/Cinis.MsSql/DapperExtensions.Async.cs
+++ Source/Cinis.MsSql/DapperExtensions.Async.cs
@@ -1,5 +1,23 @@
-﻿namespace Cinis.MsSql;
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Cinis.MsSql;
 
 public static partial class DapperExtensions
 {
+    public static async Task<dynamic> CreateAsync<T>(this SqlConnection connection, T entity, SqlTransaction? transaction = null, DbType dbType = DbType.Int32)
+    {
+        if (connection is null)
+        {
+            throw new ArgumentNullException(nameof(connection));
+        }
+
+        var stringOfColumns = string.Join(", ", GetColumns<T>());
+        var stringOfParameters = string.Join(", ", GetColumnPropertyNames<T>().Select(e => "@" + e));
+        var sql = $"insert into {GetTableSchema<T>()}.{GetTableName<T>()} ({stringOfColumns}) values ({stringOfParameters}) returning {GetPrimaryKey<T>()?.Name}";
+
+        var result = await connection.ExecuteAsync(sql, entity, transaction);
+        return result;
+    }
 }
