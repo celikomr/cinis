e75281e6f1f36ad5234e127c0a3417096f080064
Ömer Çelik <oclk@outlook.com.tr>
2022-10-23 13:49:10 +0300

    feat: Developed Delete func in Cinis.MsSql project

--- Source/Cinis.MsSql/DapperExtensions.cs
+++ Source/Cinis.MsSql/DapperExtensions.cs
@@ -94,4 +94,25 @@ public static partial class DapperExtensions
         var result = connection.Execute(sql, entity, transaction);
         return result;
     }
+
+    public static dynamic Delete<T>(this SqlConnection connection, string? whereClause = null, SqlTransaction? transaction = null)
+    {
+        if (connection is null)
+        {
+            throw new ArgumentNullException(nameof(connection));
+        }
+
+        string sql;
+        if (string.IsNullOrEmpty(whereClause))
+        {
+            sql = $"delete from {GetTableSchema<T>()}.{GetTableName<T>()}";
+        }
+        else
+        {
+            sql = $"delete from {GetTableSchema<T>()}.{GetTableName<T>()} where {whereClause}";
+        }
+
+        var result = connection.Execute(sql, null, transaction);
+        return result;
+    }
 }
