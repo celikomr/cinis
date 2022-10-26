1cf79b949d8b5a75ccde9aa45089418332ead2c5
Ömer Çelik <oclk@outlook.com.tr>
2022-10-26 21:36:04 +0300

    refactor: Updated MySql extensions with new id parameters

--- Source/Cinis.MySql/DapperExtensions.cs
+++ Source/Cinis.MySql/DapperExtensions.cs
@@ -54,7 +54,7 @@ public static partial class DapperExtensions
         return result;
     }
 
-    public static List<T> Read<T>(this MySqlConnection connection, string? whereClause = null, MySqlTransaction? transaction = null)
+    public static List<T> Read<T>(this MySqlConnection connection, dynamic? id = null, string ? whereClause = null, MySqlTransaction? transaction = null)
     {
         if (connection is null)
         {
@@ -62,7 +62,11 @@ public static partial class DapperExtensions
         }
 
         string sql;
-        if (!string.IsNullOrEmpty(whereClause))
+        if (id != null)
+        {
+            sql = $"select * from {GetTableSchema<T>()}.{GetTableName<T>()} where {GetPrimaryKey<T>()?.GetCustomAttribute<ColumnAttribute>()?.Name} = @{GetPrimaryKey<T>()?.Name}";
+        }
+        else if (!string.IsNullOrEmpty(whereClause))
         {
             sql = $"select * from {GetTableSchema<T>()}.{GetTableName<T>()} where {whereClause}";
         }
@@ -85,12 +89,18 @@ public static partial class DapperExtensions
         string stringOfSets;
         if (nullable)
         {
-            stringOfSets = string.Join(", ", GetProperties<T>().Where(e => e.GetCustomAttribute<ColumnAttribute>() != null).Select(e => $"{e.GetCustomAttribute<ColumnAttribute>().Name} = @{e.Name}"));
+            stringOfSets = string.Join(", ", GetProperties<T>().Where(e => e.GetCustomAttribute<ColumnAttribute>() != null).Select(e => $"{e?.GetCustomAttribute<ColumnAttribute>()?.Name} = @{e?.Name}"));
         }
         else
         {
-            string[] propertyNames = entity.GetType().GetProperties().Where(x => x.GetCustomAttribute<ColumnAttribute>() != null && x.GetValue(entity) != null).Select(x => x.GetCustomAttribute<ColumnAttribute>().Name).ToArray();
-            stringOfSets = string.Join(" , ", propertyNames.Select(propertyName => propertyName + " = @" + entity.GetType().GetProperties().Where(x => x.GetCustomAttribute<ColumnAttribute>() != null && x.GetCustomAttribute<ColumnAttribute>().Name == propertyName).Select(e => e.Name).FirstOrDefault()));
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+#pragma warning disable CS8619 // Nullability of reference types in value doesn't match target type.
+            string[] propertyNames = entity.GetType().GetProperties()
+                                           .Where(x => x.GetCustomAttribute<ColumnAttribute>() != null && x.GetValue(entity) != null)
+                                           .Select(x => x.GetCustomAttribute<ColumnAttribute>().Name).ToArray();
+#pragma warning restore CS8619 // Nullability of reference types in value doesn't match target type.
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            stringOfSets = string.Join(" , ", propertyNames.Select(propertyName => propertyName + " = @" + entity.GetType().GetProperties().Where(x => x.GetCustomAttribute<ColumnAttribute>() != null && x?.GetCustomAttribute<ColumnAttribute>()?.Name == propertyName).Select(e => e.Name).FirstOrDefault()));
         }
 
         string sql;
