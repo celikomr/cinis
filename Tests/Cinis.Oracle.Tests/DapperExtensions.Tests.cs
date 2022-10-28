9935e1834fc8080ca7bc3320e044afaa84ba926c
Ömer Çelik <oclk@outlook.com.tr>
2022-10-28 14:02:14 +0300

    feat: Added Create_WithTransaction and Create_WithoutTransaction test funcs ti oracle test project

--- Tests/Cinis.Oracle.Tests/DapperExtensions.Tests.cs
+++ Tests/Cinis.Oracle.Tests/DapperExtensions.Tests.cs
@@ -1,9 +1,27 @@
-﻿using Oracle.ManagedDataAccess.Client;
+﻿using Cinis.Oracle.Tests.Models;
+using Oracle.ManagedDataAccess.Client;
 
 namespace Cinis.Oracle.Tests;
 
 public partial class DapperExtensions
 {
+    [Fact]
+    public void Create_WithoutTransaction()
+    {
+        string connStr = "";
+        using var connection = new OracleConnection(connStr);
+        connection.Open();
+        try
+        {
+            int postId = connection.Create(new Post("Test title - 1", "Test body - 1"));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
+
     [Fact]
     public void Create_WithTransaction()
     {
@@ -13,7 +31,10 @@ public partial class DapperExtensions
         using OracleTransaction transaction = connection.BeginTransaction();
         try
         {
-            connection.Create(new Person("John", "Cartem", 21, DateTime.Now));
+            int postId = connection.Create(new Post("Test title - 1", "Test body - 1"), transaction);
+            connection.Create(new Comment(postId, "Test name - 1", "Test email - 1", "Test body - 1"), transaction);
+            connection.Create(new Comment(postId, "Test name - 2", "Test email - 2", "Test body - 2"), transaction);
+            connection.Create(new Comment(postId, "Test name - 3", "Test email - 3", "Test body - 3"), transaction);
             transaction.Commit();
         }
         catch (Exception ex)
