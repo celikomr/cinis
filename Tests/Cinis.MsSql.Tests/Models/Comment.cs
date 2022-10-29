849f9c06edd2aec0952a02f6e4f09e899826affc
Ömer Çelik <oclk@outlook.com.tr>
2022-10-29 22:45:12 +0300

    feat: Added models in mssql test project

---
+++ Tests/Cinis.MsSql.Tests/Models/Comment.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinis.MsSql.Tests.Models;
+
+[Table("Comment")]
+public class Comment
+{
+    [Key]
+    [Column("Id")]
+    public int Id { get; set; }
+
+    [Column("PostId")]
+    public int PostId { get; set; }
+
+    [Column("Name")]
+    public string? Name { get; set; }
+
+    [Column("Email")]
+    public string? Email { get; set; }
+
+    [Column("Body")]
+    public string? Body { get; set; }
+
+    public Comment() { }
+
+    public Comment(int postId, string? name, string? email, string? body)
+    {
+        PostId = postId;
+        Name = name;
+        Email = email;
+        Body = body;
+    }
+
+    public Comment(int id, int postId, string? name, string? email, string? body)
+    {
+        Id = id;
+        PostId = postId;
+        Name = name;
+        Email = email;
+        Body = body;
+    }
+}
---
+++ Tests/Cinis.MsSql.Tests/Models/Post.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinis.MsSql.Tests.Models;
+
+[Table("Post")]
+public class Post
+{
+    [Key]
+    [Column("Id")]
+    public int Id { get; set; }
+
+    [Column("Title")]
+    public string? Title { get; set; }
+
+    [Column("Body")]
+    public string? Body { get; set; }
+
+    public List<Comment>? Comments { get; set; }
+
+    public Post() { }
+
+    public Post(string? title, string? body)
+    {
+        Title = title;
+        Body = body;
+    }
+
+    public Post(int id, string? title, string? body)
+    {
+        Id = id;
+        Title = title;
+        Body = body;
+    }
+}
