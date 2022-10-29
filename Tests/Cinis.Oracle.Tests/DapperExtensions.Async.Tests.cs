505b8eeb9a7a5ff8c89778e3c3665f6eb7248ec5
Ömer Çelik <oclk@outlook.com.tr>
2022-10-29 13:49:04 +0300

    feat: Created CreateAsync_WithoutTransaction method in oracle test project

---
+++ Tests/Cinis.Oracle.Tests/DapperExtensions.Async.Tests.cs
@@ -0,0 +1,23 @@
+﻿using Cinis.Oracle.Tests.Models;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Cinis.Oracle.Tests;
+
+public partial class DapperExtensions
+{
+    [Fact]
+    public async Task CreateAsync_WithoutTransaction()
+    {
+        using var connection = new OracleConnection(ConnectionString);
+        connection.Open();
+        try
+        {
+            int postId = await connection.CreateAsync(new Post("Test title - 1", "Test body - 1"));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
+}
