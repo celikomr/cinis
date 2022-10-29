47db74693e382d89f2e9154f9fc5d09a0e1ab5ac
Ömer Çelik <oclk@outlook.com.tr>
2022-10-29 14:48:15 +0300

    feat: Added models in mysql test project

---
+++ Tests/Cinis.MySql.Tests/Models/Comment.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinis.MySql.Tests.Models;
+
+[Table("\"COMMENT\"", Schema = "oclk")]
+public class Comment
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [Column("POST_ID")]
+    public int PostId { get; set; }
+
+    [Column("NAME")]
+    public string? Name { get; set; }
+
+    [Column("EMAIL")]
+    public string? Email { get; set; }
+
+    [Column("BODY")]
+    public string? Body { get; set; }
+
+    public Comment() { }
+
+    public Comment(int postId, string? name, string? email, string? body)
+    {
+        PostId = postId;
+        Name = name;
+        Email = email;
+        Body = body;
+    }
+
+    public Comment(int id, int postId, string? name, string? email, string? body)
+    {
+        Id = id;
+        PostId = postId;
+        Name = name;
+        Email = email;
+        Body = body;
+    }
+}
---
+++ Tests/Cinis.MySql.Tests/Models/Post.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cinis.MySql.Tests.Models;
+
+[Table("POST", Schema = "OCLK")]
+public class Post
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [Column("TITLE")]
+    public string? Title { get; set; }
+
+    [Column("BODY")]
+    public string? Body { get; set; }
+
+    public List<Comment>? Comments { get; set; }
+
+    public Post() { }
+
+    public Post(string? title, string? body)
+    {
+        Title = title;
+        Body = body;
+    }
+
+    public Post(int id, string? title, string? body)
+    {
+        Id = id;
+        Title = title;
+        Body = body;
+    }
+}
