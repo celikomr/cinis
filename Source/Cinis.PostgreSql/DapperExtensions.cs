05d11963516d968c24dd40329ff2b7365d0bd613
Ömer Çelik <oclk@outlook.com.tr>
2022-10-29 18:29:04 +0300

    refactor: Changed id parameters in postgres

--- Source/Cinis.PostgreSql/DapperExtensions.cs
+++ Source/Cinis.PostgreSql/DapperExtensions.cs
@@ -54,7 +54,7 @@ public static partial class DapperExtensions
         return result;
     }
 
-    public static List<T> Read<T>(this NpgsqlConnection connection, dynamic? id = null, string ? whereClause = null, NpgsqlTransaction? transaction = null)
+    public static List<T> Read<T>(this NpgsqlConnection connection, dynamic? id = null, string? whereClause = null, NpgsqlTransaction? transaction = null)
     {
         if (connection is null)
         {
@@ -64,7 +64,7 @@ public static partial class DapperExtensions
         string sql;
         if (id != null)
         {
-            sql = $"select * from {GetTableSchema<T>()}.{GetTableName<T>()} where {GetPrimaryKey<T>()?.GetCustomAttribute<ColumnAttribute>()?.Name} = @{GetPrimaryKey<T>()?.Name}";
+            sql = $"select * from {GetTableSchema<T>()}.{GetTableName<T>()} where {GetPrimaryKey<T>()?.GetCustomAttribute<ColumnAttribute>()?.Name} = {id}";
         }
         else if (!string.IsNullOrEmpty(whereClause))
         {
@@ -75,7 +75,7 @@ public static partial class DapperExtensions
             sql = $"select * from {GetTableSchema<T>()}.{GetTableName<T>()}";
         }
 
-        List<T> result = connection.Query<T>(sql, null, transaction).ToList();
+        List<T> result = connection.Query<T>(sql, transaction: transaction).ToList();
         return result;
     }
 
@@ -117,7 +117,7 @@ public static partial class DapperExtensions
         return result;
     }
 
-    public static dynamic Delete<T>(this NpgsqlConnection connection, string? whereClause = null, NpgsqlTransaction? transaction = null)
+    public static dynamic Delete<T>(this NpgsqlConnection connection, dynamic? id = null, string? whereClause = null, NpgsqlTransaction? transaction = null)
     {
         if (connection is null)
         {
@@ -125,16 +125,20 @@ public static partial class DapperExtensions
         }
 
         string sql;
-        if (string.IsNullOrEmpty(whereClause))
+        if (id != null)
         {
-            sql = $"delete from {GetTableSchema<T>()}.{GetTableName<T>()}";
+            sql = $"delete from {GetTableSchema<T>()}.{GetTableName<T>()} where {GetPrimaryKey<T>()?.GetCustomAttribute<ColumnAttribute>()?.Name} = '{id}'";
         }
-        else
+        else if (!string.IsNullOrEmpty(whereClause))
         {
             sql = $"delete from {GetTableSchema<T>()}.{GetTableName<T>()} where {whereClause}";
         }
+        else
+        {
+            sql = $"delete from {GetTableSchema<T>()}.{GetTableName<T>()}";
+        }
 
-        var result = connection.Execute(sql, null, transaction);
+        var result = connection.Execute(sql, transaction: transaction);
         return result;
     }
 }
