88b327cf791c3856332aa90bdf4a4fae65f714c0
Ömer Çelik <oclk@outlook.com.tr>
2022-10-29 13:50:19 +0300

    feat: Created CreateAsync_WithTransaction ops in oracle test

--- Tests/Cinis.Oracle.Tests/DapperExtensions.Async.Tests.cs
+++ Tests/Cinis.Oracle.Tests/DapperExtensions.Async.Tests.cs
@@ -20,4 +20,26 @@ public partial class DapperExtensions
             throw;
         }
     }
+
+    [Fact]
+    public async Task CreateAsync_WithTransaction()
+    {
+        using var connection = new OracleConnection(ConnectionString);
+        connection.Open();
+        using OracleTransaction transaction = connection.BeginTransaction();
+        try
+        {
+            int postId = await connection.CreateAsync(new Post("Test title - 1", "Test body - 1"), transaction);
+            await connection.CreateAsync(new Comment(postId, "Test name - 1", "Test email - 1", "Test body - 1"), transaction);
+            await connection.CreateAsync(new Comment(postId, "Test name - 2", "Test email - 2", "Test body - 2"), transaction);
+            await connection.CreateAsync(new Comment(postId, "Test name - 3", "Test email - 3", "Test body - 3"), transaction);
+            transaction.Commit();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            transaction.Rollback();
+            throw;
+        }
+    }
 }
