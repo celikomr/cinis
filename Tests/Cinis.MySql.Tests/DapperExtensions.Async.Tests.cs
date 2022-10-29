354a55159dd8448e0234d36e5cd4bba13d03ed58
Ömer Çelik <oclk@outlook.com.tr>
2022-10-29 18:16:01 +0300

    feat: Added async test ops in mysql test project

---
+++ Tests/Cinis.MySql.Tests/DapperExtensions.Async.Tests.cs
@@ -0,0 +1,166 @@
+﻿using Cinis.MySql.Tests.Models;
+using MySql.Data.MySqlClient;
+
+namespace Cinis.MySql.Tests;
+
+public partial class DapperExtensions
+{
+    [Fact]
+    public async Task CreateAsync_WithoutTransaction()
+    {
+        using var connection = new MySqlConnection(ConnectionString);
+        await connection.OpenAsync();
+        try
+        {
+            int postId = await connection.CreateAsync(new Post("Test title - 1", "Test body - 1"));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
+
+    [Fact]
+    public async Task CreateAsync_WithTransaction()
+    {
+        using var connection = new MySqlConnection(ConnectionString);
+        await connection.OpenAsync();
+        using MySqlTransaction transaction = connection.BeginTransaction();
+        try
+        {
+            int postId = await connection.CreateAsync(new Post("Test title - 1", "Test body - 1"), transaction);
+            await connection.CreateAsync(new Comment(postId, "Test name - 1", "Test email - 1", "Test body - 1"), transaction);
+            await connection.CreateAsync(new Comment(postId, "Test name - 2", "Test email - 2", "Test body - 2"), transaction);
+            await connection.CreateAsync(new Comment(postId, "Test name - 3", "Test email - 3", "Test body - 3"), transaction);
+            transaction.Commit();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            transaction.Rollback();
+            throw;
+        }
+    }
+
+    [Fact]
+    public async Task ReadAsync_ById()
+    {
+        using var connection = new MySqlConnection(ConnectionString);
+        await connection.OpenAsync();
+        try
+        {
+            var posts = await connection.ReadAsync<Post>(7);
+            Post? post = posts.FirstOrDefault();
+            if (post != null)
+            {
+                post.Comments = await connection.ReadAsync<Comment>(whereClause: $"post_id = '{post.Id}'");
+            }
+            Assert.NotNull(post);
+            Assert.NotNull(post?.Comments);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
+
+    [Fact]
+    public async Task ReadAsync_ByWhereClause()
+    {
+        using var connection = new MySqlConnection(ConnectionString);
+        await connection.OpenAsync();
+        try
+        {
+            List<Post> posts = await connection.ReadAsync<Post>(whereClause: $"title like '%Test title%'");
+            foreach (Post post in posts)
+            {
+                post.Comments = await connection.ReadAsync<Comment>(whereClause: $"post_id = '{post.Id}'");
+            }
+            Assert.NotNull(posts);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
+
+    [Fact]
+    public async Task UpdateAsync_ById()
+    {
+        using var connection = new MySqlConnection(ConnectionString);
+        await connection.OpenAsync();
+        try
+        {
+            var posts = await connection.ReadAsync<Post>(1000001591);
+            Post? post = posts.FirstOrDefault();
+            if (post != null)
+            {
+                post.Title = "Updated Test Title";
+                await connection.UpdateAsync(post); // Update By Id
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
+
+    [Fact]
+    public async Task UpdateAsync_ByWhereClause()
+    {
+        using var connection = new MySqlConnection(ConnectionString);
+        connection.Open();
+        try
+        {
+            var posts = await connection.ReadAsync<Post>(1000001600);
+            Post? post = posts.FirstOrDefault();
+            if (post != null)
+            {
+                post.Title = "Updated Test Title";
+                post.Body = null;
+                await connection.UpdateAsync(post, true, $"id = '{post.Id}'"); // Update By WhereClause
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
+
+    [Fact]
+    public async void DeleteAsync_ById()
+    {
+        using var connection = new MySqlConnection(ConnectionString);
+        await connection.OpenAsync();
+        try
+        {
+            await connection.DeleteAsync<Post>(4); // dynamic id = '1000001633'
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
+
+    [Fact]
+    public async Task DeleteAsync_ByWhereClause()
+    {
+        using var connection = new MySqlConnection(ConnectionString);
+        await connection.OpenAsync();
+        try
+        {
+            await connection.DeleteAsync<Post>(whereClause: "id = '5'"); // Delete By WhereClause
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+            throw;
+        }
+    }
+}
