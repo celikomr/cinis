23d734154586f2c5a18bcb5f451656c5aee41c51
Ömer Çelik <oclk@outlook.com.tr>
2022-11-02 23:54:21 +0300

    refactor: Removed primary key from update sql in mssql project

--- Source/Cinis.MsSql/DapperExtensions.Async.cs
+++ Source/Cinis.MsSql/DapperExtensions.Async.cs
@@ -58,18 +58,18 @@ public static partial class DapperExtensions
         string stringOfSets;
         if (nullable)
         {
-            stringOfSets = string.Join(", ", GetProperties<T>().Where(e => e.GetCustomAttribute<ColumnAttribute>() != null).Select(e => $"{e?.GetCustomAttribute<ColumnAttribute>()?.Name} = @{e?.Name}"));
+            stringOfSets = string.Join(", ", GetProperties<T>().Where(e => e?.GetCustomAttribute<ColumnAttribute>() != null && e?.GetCustomAttribute<ColumnAttribute>()?.Name != GetPrimaryKey<T>()?.Name).Select(e => $"{e?.GetCustomAttribute<ColumnAttribute>()?.Name} = @{e?.Name}"));
         }
         else
         {
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
 #pragma warning disable CS8619 // Nullability of reference types in value doesn't match target type.
             string[] propertyNames = entity.GetType().GetProperties()
-                                           .Where(x => x.GetCustomAttribute<ColumnAttribute>() != null && x.GetValue(entity) != null)
-                                           .Select(x => x.GetCustomAttribute<ColumnAttribute>().Name).ToArray();
+                                           .Where(x => x?.GetCustomAttribute<ColumnAttribute>() != null && x?.GetCustomAttribute<ColumnAttribute>()?.Name != GetPrimaryKey<T>()?.Name && x?.GetValue(entity) != null)
+                                           .Select(x => x?.GetCustomAttribute<ColumnAttribute>().Name).ToArray();
 #pragma warning restore CS8619 // Nullability of reference types in value doesn't match target type.
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
-            stringOfSets = string.Join(" , ", propertyNames.Select(propertyName => propertyName + " = @" + entity.GetType().GetProperties().Where(x => x.GetCustomAttribute<ColumnAttribute>() != null && x?.GetCustomAttribute<ColumnAttribute>()?.Name == propertyName).Select(e => e.Name).FirstOrDefault()));
+            stringOfSets = string.Join(" , ", propertyNames.Select(propertyName => propertyName + " = @" + entity.GetType().GetProperties().Where(x => x?.GetCustomAttribute<ColumnAttribute>() != null && x?.GetCustomAttribute<ColumnAttribute>()?.Name == propertyName).Select(e => e?.Name).FirstOrDefault()));
         }
 
         string sql;
