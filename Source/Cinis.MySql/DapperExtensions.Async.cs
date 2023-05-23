7ed121c4d58266c34c2cf5f4bf86198f4e494057
Ömer Çelik <oclk@outlook.com.tr>
2023-05-23 10:41:32 +0300

    fix: Match Underscores Issue and PostgreSql Returning Issue for MySql

--- Source/Cinis.MySql/DapperExtensions.Async.cs
+++ Source/Cinis.MySql/DapperExtensions.Async.cs
@@ -14,11 +14,13 @@ public static partial class DapperExtensions
             throw new ArgumentNullException(nameof(connection));
         }
 
+        DefaultTypeMap.MatchNamesWithUnderscores = true;
+
         var stringOfColumns = string.Join(", ", GetColumns<T>());
         var stringOfParameters = string.Join(", ", GetColumnPropertyNames<T>().Select(e => "@" + e));
         var sql = $"insert into {GetTableSchema<T>()}{GetTableName<T>()} ({stringOfColumns}) values ({stringOfParameters}); select LAST_INSERT_ID();";
 
-        var result = await connection.ExecuteAsync(sql, entity, transaction);
+        var result = await connection.ExecuteScalarAsync(sql, entity, transaction);
         return result;
     }
 	
